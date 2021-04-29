3db60332f69322caec2981b0ad15e6f3ccd8cad9
redfernj-OSU <81777875+redfernj-OSU@users.noreply.github.com>
2021-04-29 16:42:32 -0700

    calc

---
+++ test_calc.py
@@ -0,0 +1,14 @@
+import calc
+import pytest
+
+def test_add():
+    assert calc.add(2,2) == 4
+
+def test_subtract():
+    assert calc.subtract(2,2) == 0
+
+def test_multiply():
+    assert calc.multiply(2,2) == 4
+
+def test_divide():
+    assert calc.divide(2,2) == 1
